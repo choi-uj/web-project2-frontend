6d7de49eae8c6c80d5c19042b5bc0f13a9a3531d
choi ej <cyslisa0211@naver.com>
2025-10-01 14:26:06 +0900

    251001-7

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-project2-frontend/',
+  base: './',
   server: {
     proxy: {
       // '/api': 'http://localhost:8500',  // 백엔드 서버 API 경로 설정
